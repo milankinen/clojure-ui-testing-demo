5e96ecf5c1fb01dc7e72a91f4d9c64694fdd1625
Matti Lankinen <m.lankinen@iki.fi>
2019-02-13 17:43:01 +0200

    Load initial todos from API

--- ui/src/client.js
+++ ui/src/client.js
@@ -1,29 +1,32 @@
-import 'todomvc-common';
-import TodoStore from './stores/TodoStore';
-import ViewStore from './stores/ViewStore';
-import TodoApp from './components/todoApp.js';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "todomvc-common";
+import TodoStore from "./stores/TodoStore";
+import ViewStore from "./stores/ViewStore";
+import TodoApp from "./components/todoApp.js";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const initialState = window.initialState && JSON.parse(window.initialState) || {};
+fetch("/api/todos")
+  .then(res => res.json())
+  .then(todos => {
+    var todoStore = TodoStore.fromJS(todos || []);
+    var viewStore = new ViewStore();
 
-var todoStore = TodoStore.fromJS(initialState.todos || []);
-var viewStore = new ViewStore();
+    todoStore.subscribeServerToStore();
 
-todoStore.subscribeServerToStore();
-
-ReactDOM.render(
-	<TodoApp todoStore={todoStore} viewStore={viewStore}/>,
-	document.getElementById('todoapp')
-);
-
-if (module.hot) {
-  module.hot.accept('./components/todoApp', () => {
-    var NewTodoApp = require('./components/todoApp').default;
     ReactDOM.render(
-      <NewTodoApp todoStore={todoStore} viewStore={viewStore}/>,
-      document.getElementById('todoapp')
+      <TodoApp todoStore={todoStore} viewStore={viewStore} />,
+      document.getElementById("todoapp")
     );
   });
-}
 
+if (process.env.NODE_ENV !== "production") {
+  if (module.hot) {
+    module.hot.accept("./components/todoApp", () => {
+      var NewTodoApp = require("./components/todoApp").default;
+      ReactDOM.render(
+        <NewTodoApp todoStore={todoStore} viewStore={viewStore} />,
+        document.getElementById("todoapp")
+      );
+    });
+  }
+}
--- ui/src/server.js
+++ ui/src/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import path from 'path';
-import escape from 'jsesc';
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import escape from "jsesc";
 
 // import { renderToString } from 'react-dom/server'
 //
@@ -11,32 +11,33 @@ import escape from 'jsesc';
 // import React from 'react';
 
 const app = express();
-app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')))
+app.use(
+  "/node_modules",
+  express.static(path.join(__dirname, "../node_modules"))
+);
 
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('../webpack.config');
+const webpack = require("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
+const config = require("../webpack.config");
 const compiler = webpack(config);
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(
+  webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  })
+);
 app.use(webpackHotMiddleware(compiler));
 
 const renderFullPage = html => {
-	const initialState = { todos };
-	const initialStateJSON = escape( // So safe!
-		JSON.stringify(initialState),
-		{ wrap: true, isScriptContext: true, json: true }
-	);
-	return `
+  const initialState = { todos };
+  return `
 	<!doctype html>
 	<html lang="en">
 		<head>
 			<meta charset="UTF-8">
 			<link rel="stylesheet" href="/node_modules/todomvc-common/base.css">
 			<link rel="stylesheet" href="/node_modules/todomvc-app-css/index.css">
-			<script>
-				window.initialState = ${initialStateJSON}
-			</script>
 		</head>
 		<body>
 			<section id="todoapp" class="todoapp">${html}</section>
@@ -49,50 +50,61 @@ const renderFullPage = html => {
 			</footer>
 		</body>
 	</html>
-	`
+	`;
 };
 
 let todos = []; // Todos are stored here
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-	// const todoStore = TodoStore.fromJS(todos);
-	// const viewStore = new ViewStore();
+app.get("/", function(req, res) {
+  // const todoStore = TodoStore.fromJS(todos);
+  // const viewStore = new ViewStore();
   //
-	// const initView = renderToString((
-	// 	<TodoApp todoStore={todoStore} viewStore={viewStore} />
-	// ));
+  // const initView = renderToString((
+  // 	<TodoApp todoStore={todoStore} viewStore={viewStore} />
+  // ));
   //
-	const page = renderFullPage('');
+  const page = renderFullPage("");
 
-	res.status(200).send(page);
+  res.status(200).send(page);
 });
 
-app.post('/api/todos', function(req, res) {
-	todos = req.body.todos;
-	if (Array.isArray(todos)) {
-		console.log(`Updated todos (${todos.length})`);
-		res.status(201).send(JSON.stringify({ success: true }));
-	} else {
-		res.status(200).send(JSON.stringify({ success: false, error: "expected `todos` to be array" }));
-	}
+app.get("/api/todos", function(req, res) {
+  res.status(200).json(todos);
 });
 
-app.get('*', function(req, res) {
-	res.status(404).send('Server.js > 404 - Page Not Found');
+app.post("/api/todos", function(req, res) {
+  todos = req.body.todos;
+  if (Array.isArray(todos)) {
+    console.log(`Updated todos (${todos.length})`);
+    res.status(201).send(JSON.stringify({ success: true }));
+  } else {
+    res
+      .status(200)
+      .send(
+        JSON.stringify({
+          success: false,
+          error: "expected `todos` to be array"
+        })
+      );
+  }
+});
+
+app.get("*", function(req, res) {
+  res.status(404).send("Server.js > 404 - Page Not Found");
 });
 
 app.use((err, req, res, next) => {
-	console.error("Error on request %s %s", req.method, req.url);
-	console.error(err.stack);
-	res.status(500).send("Server error");
+  console.error("Error on request %s %s", req.method, req.url);
+  console.error(err.stack);
+  res.status(500).send("Server error");
 });
 
-process.on('uncaughtException', evt => {
-	console.log('uncaughtException: ', evt);
+process.on("uncaughtException", evt => {
+  console.log("uncaughtException: ", evt);
 });
 
-app.listen(3000, function(){
-	console.log('Listening on port 3000');
+app.listen(3000, function() {
+  console.log("Listening on port 3000");
 });
